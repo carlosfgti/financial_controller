6e3750708c2f9df7dfc593d1e5a57094e2eac40a
Carlos Ferreira <conta@especializati.com.br>
2019-04-04 11:39:32 -0300

    Simplifying use express

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,4 @@
-const express = require('express')
-
-const app = express()
+const app = require('express')()
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listen port ${port}`))
