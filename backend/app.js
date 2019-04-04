f87c257a8e951201409705b6217cd3ccdaa9c45a
Carlos Ferreira <conta@especializati.com.br>
2019-04-04 11:46:03 -0300

    consign load project folders

--- backend/app.js
+++ backend/app.js
@@ -1,4 +1,12 @@
 const app = require('express')()
+const consign = require('consign')
+
+consign()
+    .include('./middlewares')
+    .then('./controllers')
+    .then('./models')
+    .then('./routes')
+    .into(app)
 
 const port = process.env.PORT || 3000;
 app.listen(port, () => console.log(`Listen port ${port}`))
