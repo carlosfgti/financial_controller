98aefb5074795dfaf95a1d1aa9dde7ceb9f7ea33
Carlos Ferreira <conta@especializati.com.br>
2019-04-08 10:39:08 -0300

    encrypted password

--- backend/controllers/auth.js
+++ backend/controllers/auth.js
@@ -1,12 +1,21 @@
-
+const bcrypt = require('bcrypt-nodejs')
 
 // const User = require('../models/user')
 
 module.exports = app => {
     const User = app.models.user
 
+    const encryptPassword = password => {
+        const salt = bcrypt.genSaltSync(10)
+
+        return bcrypt.hashSync(password, salt)
+    }
+
     const signup = (req, res, next) => {
-        const user = new User({... req.query})
+        let data = {... req.query}
+        data.password = encryptPassword(data.password)
+
+        const user = new User(data)
         user.save()
             .then(result => {
                 return res.status(201).json({
